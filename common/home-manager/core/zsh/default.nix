1193016d1f4e1479ce82ddb51188ee11b712c8ab
Matthew Spangler <mattspangler@protonmail.com>
2025-05-29 15:46:19 -0500

    added starship to zsh

--- common/home-manager/core/zsh/default.nix
+++ common/home-manager/core/zsh/default.nix
@@ -1,41 +1,43 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
-    oh-my-zsh = {
+  programs = {
+    zsh = {
       enable = true;
-      plugins = [
-        "git"
-        "sudo"
-        "docker"
-        "z"
-        "history"
-      "npm"
-      "node"
-      "rust"
-      "deno"
-      ];
-      theme = "spaceship";
-    };
-    shellAliases = {
-      ll = "ls -la";
-      pip = "pip3";
-      nrs-gaming-desktop = "sudo nixos-rebuild switch --flake ~/nix-config/#gaming-desktop --option binary-caches-parallel-connections 5";
-      der = "~/.config/emacs/bin/doom build && ~/.config/emacs/bin/doom sync";
-      des = "~/.config/emacs/bin/doom sync";
-    };
-    initContent = ''
-      SPACESHIP_PROMPT_ADD_NEWLINE="true"
-      SPACESHIP_PROMPT_SEPARATE_LINE="true"
-      SPACESHIP_CHAR_SYMBOL="❯"
-      SPACESHIP_CHAR_SUFFIX=" "
-      export OPENAI_API_KEY=$(cat ${config.sops.secrets."nano-gpt_key".path})
-    '';
-  };
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      enableSyntaxHighlighting = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "sudo"
+          "z"
+          "history"
+        "npm"
+        "node"
+        "rust"
+        "deno"
+        ];
+      };
+      shellAliases = {
+        ll = "ls -la";
+        pip = "pip3";
+        nrs-gaming-desktop = "sudo nixos-rebuild switch --flake ~/nix-config/#gaming-desktop --option binary-caches-parallel-connections 5";
+        der = "~/.config/emacs/bin/doom build && ~/.config/emacs/bin/doom sync";
+        des = "~/.config/emacs/bin/doom sync";
+      };
+      initContent = ''
+        eval "$(starship init zsh)"
+        export OPENAI_API_KEY=$(cat ${config.sops.secrets."nano-gpt_key".path})
+      '';
+    }; # end zsh block
+
+    starship = {
+      enable = true;
+    }; # end starship block
+
+  }; # end programs block
 
   home.packages = with pkgs; [
     zsh
@@ -43,6 +45,7 @@
     zsh-syntax-highlighting
     zsh-autosuggestions
     zsh-history-substring-search
-    spaceship-prompt
+    starship
   ];
+
 }
