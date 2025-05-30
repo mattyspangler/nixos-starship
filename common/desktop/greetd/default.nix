091be9acf2431339263557ffab60e262896bb827
Matthew Spangler <mattspangler@protonmail.com>
2025-05-30 14:20:59 -0500

    tweaked greetd theming, tweaked inactive window color to make it pop less

--- common/desktop/greetd/default.nix
+++ common/desktop/greetd/default.nix
@@ -4,8 +4,7 @@ let
     # gtkgreet takes 15-30 seconds to start
     # https://github.com/swaywm/sway/wiki#gtk-applications-take-20-seconds-to-start
     exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
-    # `-l` activates layer-shell mode. Notice that `swaymsg exit` will run after gtkgreet.
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l; swaymsg exit"
+    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l --style /etc/greetd/greetd.css; swaymsg exit"
     bindsym Mod4+shift+e exec swaynag \
       -t warning \
       -m 'What do you want to do?' \
@@ -28,5 +27,6 @@ in
     zsh
     labwc
   '';
-}
 
+  environment.etc."greetd/greetd.css".source = ./greetd-style.css;
+}
--- common/home-manager/desktop/alacritty/default.nix
+++ common/home-manager/desktop/alacritty/default.nix
@@ -4,13 +4,14 @@
   # Alacritty configuration
   programs.alacritty = {
     enable = true;
-    settings = {
-      # Import the custom theme
-      import = [ "${pkgs.path}/share/terminfo" ];
-    };
+    #settings = {
+    #  # Import the custom theme
+    #  general.import = [ "${pkgs.path}/share/terminfo" ];
+    #};
   };
 
-  xdg.configFile."alacritty/config.toml".source = ./config.toml;
+  # TODO: this line doesn't seem to do anything. Delete, revise?
+  xdg.configFile."alacritty/alacritty.toml".source = ./config.toml;
 
   # Ensure Alacritty is installed
   home.packages = with pkgs; [ alacritty ];
