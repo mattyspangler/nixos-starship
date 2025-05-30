4bd3421022d62c879295922f1be4a9a6114c68ef
Matthew Spangler <mattspangler@protonmail.com>
2025-05-30 13:22:15 -0500

    tweaking waybar colors

--- common/home-manager/desktop/alacritty/default.nix
+++ common/home-manager/desktop/alacritty/default.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }:
 
 {
+  # Alacritty configuration
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      # Import the custom theme
+      import = [ "${pkgs.path}/share/terminfo" ];
+    };
+  };
+
   xdg.configFile."alacritty/config.toml".source = ./config.toml;
+
+  # Ensure Alacritty is installed
+  home.packages = with pkgs; [ alacritty ];
 }
