1adb41c12677d0b2d2036c37fb754464793308d8
Matthew Spangler <mattspangler@protonmail.com>
2025-09-25 23:39:59 -0500

    placeholder file contents

---
+++ machines/secure-librem5/home-manager/sxmo/default.nix
@@ -0,0 +1,76 @@
+{
+  pkgs,
+  ...
+}:
+{
+
+  # home = {
+  #   # All packages relevant to my Sway environment:
+  #   packages = with pkgs; [
+  #     sway
+  #     wayland
+  #     wofi # launcher
+  #     wofi-emoji
+  #     cheat
+  #     navi
+  #     bc
+  #     numbat
+  #     #notify-osd
+  #     libnotify
+  #     libinput
+  #     #dunst # notify daemon
+  #     xdg-utils
+  #     glib
+  #     brightnessctl
+  #     pavucontrol
+  #     alacritty
+  #     playerctl
+  #     xfce.thunar # file manager
+  #     lxappearance # theme settings
+  #     wl-clipboard # clipboard manager
+  #     yad # gui dialogs for shell scripts
+  #     ydotool # generic command-line automation for wayland
+  #     wtype # xdotool type automation for wayland
+  #     espanso-wayland # text expander for wayland
+  #     swww # wallpaper changer
+  #     grim # screenshots
+  #     waybar
+  #     wdisplays # gui for configuring displays
+  #     wmctrl # needed to activate app focus for ulauncher
+  #     mako # notification manager
+  #     slurp # dimension-grabbing CLI, to use with grim
+  #     swayidle # idle manager
+  #     swaylock # screen lock
+  #     papirus-icon-theme # icon theme
+  #   ];
+
+  #   file = {
+  #     # sway
+  #     ".config/sway/50-systemd-user.conf".source = ./50-systemd-user.conf;
+
+  #     # waybar
+  #     ".config/waybar/config".source = ./waybar-config;
+  #     ".config/waybar/style.css".source = ./waybar-style.css;
+
+  #     # wofi configs
+  #     ".config/wofi/config".source = ./wofi/wofi-config;
+  #     ".config/wofi/style.css".source = ./wofi/wofi-style.css;
+  #     ".ai-wofi-settings".source = ./wofi/ai-wofi-settings;
+
+  #     # wofi scripts
+  #     ".config/wofi/hacky-wofi" = {
+  #       source = ./wofi/hacky-wofi;
+  #       executable = true;
+  #     };
+  #     ".config/wofi/ai-wofi.py" = {
+  #       source = ./wofi/ai-wofi.py;
+  #       executable = true;
+  #     };
+
+  #     # wallpaper
+  #     "nixship-wallpaper.png".source = ./wallpapers/nixship-wallpaper.png;
+  #   };
+
+  # }; # end home block
+
+}
