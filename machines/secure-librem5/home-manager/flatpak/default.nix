d588dd80d8b94d1004f9efd404a591a489ee9be8
Matthew Spangler <mattspangler@protonmail.com>
2025-10-01 23:06:11 -0500

    commenting all out except known-good

--- machines/secure-librem5/home-manager/flatpak/default.nix
+++ machines/secure-librem5/home-manager/flatpak/default.nix
@@ -19,35 +19,35 @@
 
       packages = [
         # Web Browsing
-        "com.brave.Browser"
+        #"com.brave.Browser"
         #"org.torproject.torbrowser-launcher"
         # Communication
         #"im.riot.Riot" # looks like Quadrix and nheko are working alternatives!
-        "org.briarproject.Briar"
-        "de.schmidhuberj.Flare" # Signal alternative
-        "sm.puri.Chatty" # XMPP, SMS, Matrix
+        #"org.briarproject.Briar"
+        #"de.schmidhuberj.Flare" # Signal alternative
+        #"sm.puri.Chatty" # XMPP, SMS, Matrix
         # Productivity
         #"org.onlyoffice.desktopeditors"
         #"org.libreoffice.LibreOffice"
         # Media
-        "org.videolan.VLC"
+        #"org.videolan.VLC"
         # Development
         "com.vscodium.codium"
         # Security
-        "org.keepassxc.KeePassXC"
-        "com.protonvpn.www"
+        #"org.keepassxc.KeePassXC"
+        #"com.protonvpn.www"
         "com.github.tchx84.Flatseal"
         # Utility
-        "xyz.slothlife.Jogger" # fitness app
+        #"xyz.slothlife.Jogger" # fitness app
         #"com.calibre_ebook.calibre"
-        "com.github.johnfactotum.Foliate" # ebook reader
-        "org.gabmus.gfeeds" # rss reader
-        "com.belmoussaoui.Authenticator"
-        "org.gnome.clocks"
-        "app.organicmaps.desktop"
+        #"com.github.johnfactotum.Foliate" # ebook reader
+        #"org.gabmus.gfeeds" # rss reader
+        #"com.belmoussaoui.Authenticator"
+        #"org.gnome.clocks"
+        #"app.organicmaps.desktop"
         "me.kozec.syncthingtk"
         # Virtualization
-        "io.podman_desktop.PodmanDesktop"
+        #"io.podman_desktop.PodmanDesktop"
         #"org.gnome.Boxes"
       ];
 
