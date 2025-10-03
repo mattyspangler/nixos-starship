08c988cfcde6234f141c763928d78872eab93c06
Matthew Spangler <mattspangler@protonmail.com>
2025-10-02 22:36:50 -0500

    adding semicolon

--- common/home-manager/core/zsh/default.nix
+++ common/home-manager/core/zsh/default.nix
@@ -24,7 +24,7 @@
         ll = "ls -la";
         pip = "pip3";
         nrs-gaming-desktop = "sudo nixos-rebuild switch --flake ~/nixos-starship/#gaming-desktop --option binary-caches-parallel-connections 5";
-        hms-librem = "~/nixos-starship/deploy_librem5_standalone.sh"
+        hms-librem = "~/nixos-starship/deploy_librem5_standalone.sh";
         der = "~/.config/emacs/bin/doom build && ~/.config/emacs/bin/doom sync";
         des = "~/.config/emacs/bin/doom sync";
       };
