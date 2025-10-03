f5ac2ac4b94153f93f3f07060d1f3f70b75acbdd
Matthew Spangler <mattspangler@protonmail.com>
2025-10-02 21:58:40 -0500

    resolving typo

--- common/home-manager/core/zsh/default.nix
+++ common/home-manager/core/zsh/default.nix
@@ -5,7 +5,7 @@
     zsh = {
       enable = true;
       enableCompletion = true;
-      autosuggestions.enable = true;
+      autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
       oh-my-zsh = {
         enable = true;
