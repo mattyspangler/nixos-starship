b345242c65ea4416df8ab75fe39fa2b6f4312b07
Matthew Spangler <mattspangler@protonmail.com>
2025-10-03 16:03:14 -0500

    trying this for tuba

--- machines/secure-librem5/home-manager/default.nix
+++ machines/secure-librem5/home-manager/default.nix
@@ -69,7 +69,7 @@
     extraPortals = with pkgs; [
       xdg-desktop-portal-wlr
       #      xdg-desktop-portal-kde
-      #      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gtk
     ];
   };
 
