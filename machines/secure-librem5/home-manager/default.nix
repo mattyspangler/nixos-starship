5bc908375ebfa8c79a5328ba75f4255e8e4a2402
Matthew Spangler <mattspangler@protonmail.com>
2025-10-03 22:46:33 -0500

    removing let

--- machines/secure-librem5/home-manager/default.nix
+++ machines/secure-librem5/home-manager/default.nix
@@ -1,72 +1,70 @@
 { config, lib, pkgs, ... }:
-let
-  {
-    imports = [
-      ./bash
-      ./flatpak
-      ./sxmo
+{
+  imports = [
+    ./bash
+    ./flatpak
+    ./sxmo
+  ];
+
+  home = {
+    username = "nebula";
+    homeDirectory = "/home/nebula";
+
+    stateVersion = "24.05";
+
+    packages = with pkgs; [
+      zsh
+      vim
+      podman
+      waydroid
+      alacritty
+      mat2
+      # Mobile specific
+      megapixels
+      # wallet
+      feather
+      # Emacs dependencies:
+      libtool
+      cmake
+      zstd
+      # Security tools
+      clamav
+      clamtk
+      lynis
+      aide
+      fail2ban
+      keepassxc
+      #ufw #not available?
+      opensnitch
+      #chrootkit #not available?
+      #rkhunter #not available?
+      profanity # cli xmpp client
+      signal-cli
+      weechat
+      vscodium
+      # AI
+      aichat
+      yai
     ];
-  
-    home = {
-      username = "nebula";
-      homeDirectory = "/home/nebula";
-  
-      stateVersion = "24.05";
-  
-      packages = with pkgs; [
-        zsh
-        vim
-        podman
-        waydroid
-        alacritty
-        mat2
-        # Mobile specific
-        megapixels
-        # wallet
-        feather
-        # Emacs dependencies:
-        libtool
-        cmake
-        zstd
-        # Security tools
-        clamav
-        clamtk
-        lynis
-        aide
-        fail2ban
-        #ufw #not available?
-        opensnitch
-        #chrootkit #not available?
-        #rkhunter #not available?
-        profanity # cli xmpp client
-        signal-cli
-        weechat
-        vscodium
-        # AI
-        aichat
-        yai
-        # Secret service for Tuba and other apps
-        keepassxc
-      ];
-  
-    }; # end home block
-  
-    home.sessionVariables = {
-      XDG_DATA_DIRS = "$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS";
-      LANG = "en_US.UTF-8";
-      LC_TYPE = "en_US.UTF-8";
-      LC_ALL = "en_US.UTF-8";
-      XDG_CURRENT_DESKTOP = "sway";
-      GTK_USE_PORTAL = "1";
-    };
-  
-    programs.zsh.enable = true;
-  
-    programs.keepassxc = {
-      enable = true;
-      enableSecretService = true;
-    };
-  
+
+  }; # end home block
+
+  home.sessionVariables = {
+    XDG_DATA_DIRS = "$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS";
+    LANG = "en_US.UTF-8";
+    LC_TYPE = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    XDG_CURRENT_DESKTOP = "sway";
+    GTK_USE_PORTAL = "1";
+  };
+
+  programs.zsh.enable = true;
+
+  programs.keepassxc = {
+    enable = true;
+    enableSecretService = true;
+  };
+
     # Required to install flatpak
   xdg.portal = {
     enable = true;
