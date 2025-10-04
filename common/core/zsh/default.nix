5b94c4af2f438ea6eeebee511d9fb53040be0d1e
Matthew Spangler <mattspangler@protonmail.com>
2025-10-04 11:09:40 -0500

    fixing aliases

--- common/core/zsh/default.nix
+++ common/core/zsh/default.nix
@@ -16,7 +16,7 @@
           pip = "pip3";
           # I hate working with long nixos rebuild commands!
           # Mnemonic: nrs = nixos-rebuild + switch
-          nrs-gaming-desktop = "sudo nixos-rebuild switch --flake ~/nix-starship/#gaming-desktop --option binary-caches-parallel-connections 5";
+          nrs-gaming-desktop = "sudo nixos-rebuild switch --flake ~/nixos-starship/#gaming-desktop --option binary-caches-parallel-connections 5";
           # Emacs shortcuts
           # Mnemonic: der = doom emacs rebuild; des -> doom emacs sync
           der = "~/.config/emacs/bin/doom build && ~/.config/emacs/bin/doom sync";
