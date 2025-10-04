2581d89cb516cbe44cf348cb6d55f7d2c4c27a1f
Matthew Spangler <mattspangler@protonmail.com>
2025-10-03 22:43:12 -0500

    giving up on gnome-keyring for now

--- machines/secure-librem5/home-manager/default.nix
+++ machines/secure-librem5/home-manager/default.nix
@@ -1,84 +1,73 @@
 { config, lib, pkgs, ... }:
 let
-  #This is a wrapper for the gnome-keyring-daemon that adds IPC_LOCK capability.
-  # https://github.com/NixOS/nixpkgs/blob/3d2d8f281a27d466fa54b469b5993f7dde198375/nixos/modules/services/desktops/gnome3/gnome-keyring.nix#L46
-  # https://github.com/nix-community/home-manager/issues/1454
-  # https://man7.org/linux/man-pages/man2/mlock.2.html
-  # https://github.com/GNOME/gnome-keyring/blob/main/daemon/gkd-capability.c
-  # The issue is we get a `insufficient process capabilities, insecure memory might get used` error when running flatpak apps like Tuba that need gnome-keyring
-  # We already drop ambient capabilities for flatpak so we need to add a wrapper for gnome-keyring to add the IPC_LOCK capability.
-  # This is a common issue on systems where user sessions don't get this capability by default.
-  gnome-keyring-wrapped = import ./gnome-keyring-wrapper.nix { inherit pkgs; };
-in
-{
-  imports = [
-    ./bash
-    ./flatpak
-    ./sxmo
-  ];
-
-  home = {
-    username = "nebula";
-    homeDirectory = "/home/nebula";
-
-    stateVersion = "24.05";
-
-    packages = with pkgs; [
-      zsh
-      vim
-      podman
-      waydroid
-      alacritty
-      mat2
-      # Mobile specific
-      megapixels
-      # wallet
-      feather
-      # Emacs dependencies:
-      libtool
-      cmake
-      zstd
-      # Security tools
-      clamav
-      clamtk
-      lynis
-      aide
-      fail2ban
-      #ufw #not available?
-      opensnitch
-      #chrootkit #not available?
-      #rkhunter #not available?
-      profanity # cli xmpp client
-      signal-cli
-      weechat
-      vscodium
-      # AI
-      aichat
-      yai
-      # Secret service for Tuba and other apps
-      gnome-keyring-wrapped
-      seahorse
+  {
+    imports = [
+      ./bash
+      ./flatpak
+      ./sxmo
     ];
-
-  }; # end home block
-
-  home.sessionVariables = {
-    XDG_DATA_DIRS = "$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS";
-    LANG = "en_US.UTF-8";
-    LC_TYPE = "en_US.UTF-8";
-    LC_ALL = "en_US.UTF-8";
-    XDG_CURRENT_DESKTOP = "sway";
-    GTK_USE_PORTAL = "1";
-  };
-
-  programs.zsh.enable = true;
-
-  services.gnome-keyring = {
-    enable = true;
-    components = [ "secrets" "ssh" ];
-  };
-
-  # Required to install flatpak
+  
+    home = {
+      username = "nebula";
+      homeDirectory = "/home/nebula";
+  
+      stateVersion = "24.05";
+  
+      packages = with pkgs; [
+        zsh
+        vim
+        podman
+        waydroid
+        alacritty
+        mat2
+        # Mobile specific
+        megapixels
+        # wallet
+        feather
+        # Emacs dependencies:
+        libtool
+        cmake
+        zstd
+        # Security tools
+        clamav
+        clamtk
+        lynis
+        aide
+        fail2ban
+        #ufw #not available?
+        opensnitch
+        #chrootkit #not available?
+        #rkhunter #not available?
+        profanity # cli xmpp client
+        signal-cli
+        weechat
+        vscodium
+        # AI
+        aichat
+        yai
+        # Secret service for Tuba and other apps
+        keepassxc
+      ];
+  
+    }; # end home block
+  
+    home.sessionVariables = {
+      XDG_DATA_DIRS = "$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS";
+      LANG = "en_US.UTF-8";
+      LC_TYPE = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      XDG_CURRENT_DESKTOP = "sway";
+      GTK_USE_PORTAL = "1";
+    };
+  
+    programs.zsh.enable = true;
+  
+    programs.keepassxc = {
+      enable = true;
+      enableSecretService = true;
+    };
+  
+    # Required to install flatpak
   xdg.portal = {
     enable = true;
     config = {
