df827ee70a9caff1b0c532f274cd1fd14a99c602
Matthew Spangler <mattspangler@protonmail.com>
2025-10-03 22:04:39 -0500

    trying a different wrapper

--- machines/secure-librem5/home-manager/default.nix
+++ machines/secure-librem5/home-manager/default.nix
@@ -4,53 +4,65 @@
     ./bash
     ./flatpak
     ./sxmo
-    ./gnome-keyring-wrapper.nix
   ];
 
-  home = {
-    username = "nebula";
-    homeDirectory = "/home/nebula";
+  #This is a wrapper for the gnome-keyring-daemon that adds IPC_LOCK capability.
+  # https://github.com/NixOS/nixpkgs/blob/3d2d8f281a27d466fa54b469b5993f7dde198375/nixos/modules/services/desktops/gnome3/gnome-keyring.nix#L46
+  # https://github.com/nix-community/home-manager/issues/1454
+  # https://man7.org/linux/man-pages/man2/mlock.2.html
+  # https://github.com/GNOME/gnome-keyring/blob/main/daemon/gkd-capability.c
+  # The issue is we get a `insufficient process capabilities, insecure memory might get used` error when running flatpak apps like Tuba that need gnome-keyring
+  # We already drop ambient capabilities for flatpak so we need to add a wrapper for gnome-keyring to add the IPC_LOCK capability.
+  # This is a common issue on systems where user sessions don't get this capability by default.
+  let
+    gnome-keyring-wrapped = import ./gnome-keyring-wrapper.nix { inherit pkgs; };
+  in
+  {
+    home = {
+      username = "nebula";
+      homeDirectory = "/home/nebula";
 
-    stateVersion = "24.05";
+      stateVersion = "24.05";
 
-    packages = with pkgs; [
-      zsh
-      vim
-      podman
-      waydroid
-      alacritty
-      mat2
-      # Mobile specific
-      megapixels
-      # wallet
-      feather
-      # Emacs dependencies:
-      libtool
-      cmake
-      zstd
-      # Security tools
-      clamav
-      clamtk
-      lynis
-      aide
-      fail2ban
-      #ufw #not available?
-      opensnitch
-      #chrootkit #not available?
-      #rkhunter #not available?
-      profanity # cli xmpp client
-      signal-cli
-      weechat
-      vscodium
-      # AI
-      aichat
-      yai
-      # Secret service for Tuba and other apps
-    ];
+      packages = with pkgs; [
+        zsh
+        vim
+        podman
+        waydroid
+        alacritty
+        mat2
+        # Mobile specific
+        megapixels
+        # wallet
+        feather
+        # Emacs dependencies:
+        libtool
+        cmake
+        zstd
+        # Security tools
+        clamav
+        clamtk
+        lynis
+        aide
+        fail2ban
+        #ufw #not available?
+        opensnitch
+        #chrootkit #not available?
+        #rkhunter #not available?
+        profanity # cli xmpp client
+        signal-cli
+        weechat
+        vscodium
+        # AI
+        aichat
+        yai
+        # Secret service for Tuba and other apps
+        gnome-keyring-wrapped
+      ];
 
-  }; # end home block
+    }; # end home block
 
-  home.sessionVariables = {
+    home.sessionVariables = {
     XDG_DATA_DIRS = "$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS";
     LANG = "en_US.UTF-8";
     LC_TYPE = "en_US.UTF-8";
@@ -63,6 +75,7 @@
 
   services.gnome-keyring = {
     enable = true;
+    package = gnome-keyring-wrapped;
     components = [ "secrets" "ssh" ];
   };
 
--- machines/secure-librem5/home-manager/gnome-keyring-wrapper.nix
+++ machines/secure-librem5/home-manager/gnome-keyring-wrapper.nix
@@ -1,23 +1,22 @@
-{ pkgs, ... }:
+{ pkgs }:
 
 let
-  gnome-keyring-daemon-wrapped = pkgs.writeShellScriptBin "gnome-keyring-daemon" ''
+  # A wrapper for the gnome-keyring-daemon that adds IPC_LOCK capability.
+  # This is to work around issues on systems where user sessions
+  # don't get this capability by default, leading to errors like:
+  # "insufficient process capabilities, insecure memory might get used"
+  gnome-keyring-daemon-with-caps = pkgs.writeShellScriptBin "gnome-keyring-daemon" ''
     #!${pkgs.runtimeShell}
-    exec ${pkgs.util-linux}/bin/setpriv --ambient-caps '-all' ${pkgs.gnome-keyring}/bin/gnome-keyring-daemon "$@"
+    exec ${pkgs.util-linux}/bin/setpriv --clear-groups --inh-caps +ipc_lock --ambient-caps +ipc_lock -- ${pkgs.gnome-keyring}/bin/gnome-keyring-daemon "$@"
   '';
 in
-{
-  # This wrapper for the daemon should be picked up by the gnome-keyring service.
-  # It will be placed in ~/.nix-profile/bin which is in the PATH.
-  home.packages = [ gnome-keyring-daemon-wrapped ];
-
-  # Also, increase the memlock limit so the daemon doesn't need CAP_IPC_LOCK.
-  # This should address the "insecure memory might get used" warning.
-  systemd.user.services.gnome-keyring = {
-    # The service is defined by the services.gnome-keyring module.
-    # We are just overriding some settings here.
-    serviceConfig = {
-      LimitMEMLOCK = "infinity";
-    };
-  };
+# Create a new package that is a copy of gnome-keyring but with the wrapped daemon.
+# We use symlinkJoin to avoid rebuilding gnome-keyring itself.
+pkgs.symlinkJoin {
+  name = "gnome-keyring-wrapped";
+  paths = [ pkgs.gnome-keyring ];
+  postBuild = ''
+    rm $out/bin/gnome-keyring-daemon
+    ln -s ${gnome-keyring-daemon-with-caps}/bin/gnome-keyring-daemon $out/bin/gnome-keyring-daemon
+  '';
 }
