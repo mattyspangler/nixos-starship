03b7851c40950fe8b19009324badb5658ef6b043
Matthew Spangler <mattspangler@protonmail.com>
2025-10-03 22:07:12 -0500

    fixing let scope

--- machines/secure-librem5/home-manager/default.nix
+++ machines/secure-librem5/home-manager/default.nix
@@ -1,11 +1,5 @@
 { config, lib, pkgs, ... }:
-{
-  imports = [
-    ./bash
-    ./flatpak
-    ./sxmo
-  ];
-
+let
   #This is a wrapper for the gnome-keyring-daemon that adds IPC_LOCK capability.
   # https://github.com/NixOS/nixpkgs/blob/3d2d8f281a27d466fa54b469b5993f7dde198375/nixos/modules/services/desktops/gnome3/gnome-keyring.nix#L46
   # https://github.com/nix-community/home-manager/issues/1454
@@ -14,55 +8,60 @@
   # The issue is we get a `insufficient process capabilities, insecure memory might get used` error when running flatpak apps like Tuba that need gnome-keyring
   # We already drop ambient capabilities for flatpak so we need to add a wrapper for gnome-keyring to add the IPC_LOCK capability.
   # This is a common issue on systems where user sessions don't get this capability by default.
-  let
-    gnome-keyring-wrapped = import ./gnome-keyring-wrapper.nix { inherit pkgs; };
-  in
-  {
-    home = {
-      username = "nebula";
-      homeDirectory = "/home/nebula";
+  gnome-keyring-wrapped = import ./gnome-keyring-wrapper.nix { inherit pkgs; };
+in
+{
+  imports = [
+    ./bash
+    ./flatpak
+    ./sxmo
+  ];
 
-      stateVersion = "24.05";
+  home = {
+    username = "nebula";
+    homeDirectory = "/home/nebula";
 
-      packages = with pkgs; [
-        zsh
-        vim
-        podman
-        waydroid
-        alacritty
-        mat2
-        # Mobile specific
-        megapixels
-        # wallet
-        feather
-        # Emacs dependencies:
-        libtool
-        cmake
-        zstd
-        # Security tools
-        clamav
-        clamtk
-        lynis
-        aide
-        fail2ban
-        #ufw #not available?
-        opensnitch
-        #chrootkit #not available?
-        #rkhunter #not available?
-        profanity # cli xmpp client
-        signal-cli
-        weechat
-        vscodium
-        # AI
-        aichat
-        yai
-        # Secret service for Tuba and other apps
-        gnome-keyring-wrapped
-      ];
+    stateVersion = "24.05";
 
-    }; # end home block
+    packages = with pkgs; [
+      zsh
+      vim
+      podman
+      waydroid
+      alacritty
+      mat2
+      # Mobile specific
+      megapixels
+      # wallet
+      feather
+      # Emacs dependencies:
+      libtool
+      cmake
+      zstd
+      # Security tools
+      clamav
+      clamtk
+      lynis
+      aide
+      fail2ban
+      #ufw #not available?
+      opensnitch
+      #chrootkit #not available?
+      #rkhunter #not available?
+      profanity # cli xmpp client
+      signal-cli
+      weechat
+      vscodium
+      # AI
+      aichat
+      yai
+      # Secret service for Tuba and other apps
+      gnome-keyring-wrapped
+    ];
 
-    home.sessionVariables = {
+  }; # end home block
+
+  home.sessionVariables = {
     XDG_DATA_DIRS = "$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS";
     LANG = "en_US.UTF-8";
     LC_TYPE = "en_US.UTF-8";
