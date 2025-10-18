399e0364a57cf16519636536d76e7909502ffc72
Matthew Spangler <mattspangler@protonmail.com>
2025-10-18 15:13:03 -0500

    trying data server without gtk

--- machines/secure-librem5/home-manager/default.nix
+++ machines/secure-librem5/home-manager/default.nix
@@ -58,7 +58,7 @@ in
         #!${pkgs.runtimeShell}
         exec ${pkgs.util-linux}/bin/setpriv --ambient-caps '-all' ${pkgs.evolution}/bin/evolution "$@"
       '')
-      evolution-data-server-gtk4
+      evolution-data-server
       fail2ban
       feather
       fzf
