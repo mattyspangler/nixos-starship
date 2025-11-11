0758d5af6eb40a4be0ad61de596ef4369caec4fc
Matt Spangler <mattspangler@protonmail.com>
2025-11-11 17:54:32 -0600

    Trying this format

--- machines/secure-librem5/home-manager/nixpak/default.nix
+++ machines/secure-librem5/home-manager/nixpak/default.nix
@@ -1,117 +1,84 @@
-{ pkgs, lib, nixpak, ... }:
-
+{ config, lib, pkgs, nixpkgs-unstable ? null, ... }:
 let
-  # This wrapper is essential for bubblewrap to function correctly on postmarketOS
-  # by dropping ambient capabilities that interfere with its operation.
-  bwrap-without-ambient-caps = pkgs.writeShellScriptBin "bwrap" ''
-    #!${pkgs.runtimeShell}
-    exec ${pkgs.util-linux}/bin/setpriv --ambient-caps '-all' ${pkgs.bubblewrap}/bin/bwrap "$@"
-  '';
+  pkgs-unstable = if nixpkgs-unstable != null then
+    import nixpkgs-unstable {
+      system = pkgs.system;
+      config = pkgs.config;
+    }
+  else
+    pkgs; # fallback to stable if unstable not provided
+
+  mkNixPak = config.lib.nixpak.mkNixPak;
+
+  # Common sandbox configuration for messaging apps
+  messagingSandboxBase = {
+    dbus.enable = true;
+    dbus.policies = {
+      "org.freedesktop.DBus" = "talk";
+      "ca.desrt.dconf" = "talk";
+      "org.freedesktop.Notifications" = "talk";
+      "org.freedesktop.portal.Desktop" = "talk";
+    };
+
+    flatpak.enable = true;
+
+    bubblewrap = {
+      network = true; # Allow network access for messaging
+      bind.rw = [
+        (sloth.concat' sloth.homeDir "/.config")
+        (sloth.concat' sloth.homeDir "/.cache")
+        (sloth.env "XDG_RUNTIME_DIR")
+        (sloth.concat' sloth.homeDir "/.local/share")
+      ];
+      bind.ro = [
+        "/etc"
+        "/run"
+        sloth.homeDir
+      ];
+    };
+  };
+
 in
 {
-  # Import the main nixpak module to enable its features.
-  imports = [ nixpak.homeManagerModules.nixpak ];
+  # iamb - Matrix client sandbox
+  iamb-nixpak = mkNixPak {
+    config = { sloth, ... }: messagingSandboxBase // {
+      app.package = pkgs.iamb;
+      app.binPath = "bin/iamb";
 
-  # Configure nixpak to use our custom bubblewrap wrapper.
-  programs.nixpak.bubblewrap = bwrap-without-ambient-caps;
+      flatpak.appId = "com.nixpak.iamb";
 
-  # Define your sandboxed applications here.
-  # Each application will be automatically wrapped by nixpak and made available
-  # in your environment.
-  programs.nixpak.packages = {
-    # Example: Sandbox the 'neofetch' command.
-    # This basic profile provides a minimal, isolated environment.
-    neofetch = {
-      package = pkgs.neofetch;
-      profile = {
-        # The 'generic' profile is a good starting point. It provides a basic
-        # filesystem layout and isolates the application from your home directory.
-    name = "generic";
-    # We can add extra arguments to bubblewrap for more fine-grained control.
-    extraArgs = [
-      "--ro-bind" "/etc/os-release" "/etc/os-release" # Allow reading OS info
-    ];
+      bubblewrap = messagingSandboxBase.bubblewrap // {
+      # Additional iamb-specific bindings
+      bind.rw = messagingSandboxBase.bubblewrap.bind.rw ++ [
+        (sloth.concat' sloth.homeDir "/.local/state/iamb")
+      ];
+    };
   };
-};
 
-# Your specific CLI applications to sandbox
-toot = {
-  package = pkgs.toot;
-  profile = {
-    name = "generic";
-    extraArgs = [
-      "--ro-bind" "/etc/os-release" "/etc/os-release"
-    ];
-  };
-};
+  # gurk-rs - Signal client sandbox
+  gurk-rs-nixpak = mkNixPak {
+    config = { sloth, ... }: messagingSandboxBase // {
+      app.package = pkgs.gurk-rs;
+      app.binPath = "bin/gurk";
 
-iamb = {
-  package = pkgs.iamb;
-  profile = {
-    name = "generic";
-    extraArgs = [
-      "--proc" "/proc" # Allow access to process information
-    ];
-  };
-};
+      flatpak.appId = "com.nixpak.gurk-rs";
 
-gurk-rs = {
-  package = pkgs.gurk-rs;
-  profile = {
-    name = "generic";
-    extraArgs = [
-      "--ro-bind" "/etc/os-release" "/etc/os-release"
-    ];
+      bubblewrap = messagingSandboxBase.bubblewrap // {
+      # Additional gurk-rs-specific bindings
+      bind.rw = messagingSandboxBase.bubblewrap.bind.rw ++ [
+        (sloth.concat' sloth.homeDir "/.local/state/gurk-rs")
+      ];
+    };
   };
-};
 
-lynx = {
-  package = pkgs.lynx;
-  profile = {
-    name = "generic";
-    extraArgs = [
-      "--ro-bind" "/etc/os-release" "/etc/os-release"
-    ];
+  # Optional: Combined messaging sandbox with both clients
+  messaging-sandbox = pkgs.symlinkJoin {
+      name = "messaging-sandbox";
+      paths = [
+        iamb-nixpak.config.env
+        gurk-rs-nixpak.config.env
+      ];
+    };
   };
-};
-
-dillo = {
-  package = pkgs.dillo;
-  profile = {
-    name = "generic";
-    extraArgs = [
-      "--ro-bind" "/etc/os-release" "/etc/os-release"
-    ];
-  };
-};
-
-profanity = {
-  package = pkgs.profanity;
-  profile = {
-    name = "generic";
-    extraArgs = [
-      "--ro-bind" "/etc/os-release" "/etc/os-release"
-    ];
-  };
-};
-
-tuisky = {
-  package = pkgs.tuisky;
-  profile = {
-    name = "generic";
-    extraArgs = [
-      "--ro-bind" "/etc/os-release" "/etc/os-release"
-    ];
-  };
-};
-
-weechat = {
-  package = pkgs.weechat;
-  profile = {
-    name = "generic";
-    extraArgs = [
-      "--ro-bind" "/etc/os-release" "/etc/os-release"
-    ];
-  };
-};
 }
