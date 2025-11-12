768312d53833f9f31b8b1f4802ff18a91e8aa3b3
Matt Spangler <mattspangler@protonmail.com>
2025-11-11 18:55:05 -0600

    Adding bwrap fix

--- machines/secure-librem5/home-manager/nixpak/default.nix
+++ machines/secure-librem5/home-manager/nixpak/default.nix
@@ -73,5 +73,14 @@ in
   home.packages = [
     profanity-sandboxed.config.script
     iamb-sandboxed.config.script
+    # bubblewrap fails on postmarketos with sxmo-de-sway due to ambient capabilities
+    # https://gitlab.postmarketos.org/postmarketOS/pmaports/-/issues/3868
+    # https://github.com/containers/bubblewrap/issues/380
+    # https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1351
+    # Create a wrapper around bwrap to drop ambient capabilities.
+    (pkgs.writeShellScriptBin "bwrap" ''
+      #!${pkgs.runtimeShell}
+      exec ${pkgs.util-linux}/bin/setpriv --ambient-caps '-all' ${pkgs.bubblewrap}/bin/bwrap "$@"
+    '')
   ];
 }
